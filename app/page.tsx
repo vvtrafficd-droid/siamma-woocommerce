d831f0ad14a6b8dbf1b34747b6debaf37741eba9
Wagner <wagneraraujo15@gmail.com>
2025-11-17 16:29:46 -0300

    fix siamma

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import ShopPage from "./(pages)/shop/ShopPage";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <ShopPage />;
+  return (
+    <Suspense fallback={<div className="p-6 text-gray-600">Loading...</div>}>
+      <ShopPage />
+    </Suspense>
+  );
 }
